fa3dae559d2853ef3f8daa36f09bda0104a2bd7b
ModeAxe <modeaxe@gmail.com>
2019-12-18 22:25:38 -0500

    Putting old projects on GitHub

---
+++ Assets/Level2Controller_Script.cs
@@ -0,0 +1,79 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level2Controller_Script : MonoBehaviour {
+
+	public int ActualLeft;
+
+	public int ActualRight;
+
+	int SoundLeft;
+
+	int SoundRight;
+
+	// Use this for initialization
+	void Start () {
+
+		SoundLeft = 0;
+
+		SoundRight = 0;
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (ActualLeft < SoundLeft || ActualRight < SoundRight) {
+			SoundLeft = 0;
+			SoundRight = 0;
+
+            Handheld.Vibrate();
+
+			SceneManager.LoadScene ("Level 2");
+		}
+			
+		if (SoundLeft == ActualLeft && SoundRight == ActualRight) {
+
+			SceneManager.LoadScene ("Level 3");
+		}
+
+
+		if (Input.touchCount > 0){
+
+			float middle = Screen.width / 2;
+
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {	 
+
+				if (Input.GetTouch (0).position.x < middle && Input.GetTouch (0).phase == TouchPhase.Began) {
+
+					left ();
+				}
+
+				if (Input.GetTouch (0).position.x > middle && Input.GetTouch (0).phase == TouchPhase.Began) {
+
+					right ();
+				}
+			}
+
+		}
+
+
+	}
+
+	public void left(){
+		SoundLeft++;
+		Debug.Log ("Left!!");
+	}
+
+	public void right(){
+		SoundRight++;
+		Debug.Log ("Right");
+
+	
+	}
+}
